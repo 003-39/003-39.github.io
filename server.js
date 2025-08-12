d13fdd22cb451d6dfdee96f0a8cad134f64e137c
003-39 <jf79ilu@gmail.com>
2025-08-13 00:20:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,12 @@ app.get("/api/sofascore/:playerId", async (req, res) => {
   try {
     const response = await axios.get(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://www.sofascore.com/",
+        "Origin": "https://www.sofascore.com"
       },
     });
 
